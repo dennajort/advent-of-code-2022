5edc75b590d605ea0da6886fe22a4954ae24c365
JB Gosselin <1536838+dennajort@users.noreply.github.com>
2022-12-06 00:04:33 -0500

    Add day1

---
+++ day1/src/main.rs
@@ -0,0 +1,26 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() -> std::io::Result<()> {
+    let file = File::open("input.txt")?;
+    let lines = io::BufReader::new(file).lines();
+    
+    let mut calories = Vec::new();
+    let mut current_calories: u64 = 0;
+    for line in lines {
+        if let Ok(line) = line {
+            if line == "" {
+                calories.push(current_calories);
+                current_calories = 0;
+                continue;
+            } else {
+                current_calories += line.parse::<u64>().unwrap();
+            }
+        }
+    }
+    calories.sort_by(|a,b| b.cmp(a));
+    calories.truncate(3);
+    println!("{:?}", calories);
+    println!("{:?}", calories.iter().sum::<u64>());
+    Ok(())
+}
